97bf4b682434d722a28b42a7d02bb0543780deb8
kentforth <yashaar@mail.ru>
2021-01-14 22:04:50 +0700

    change file

--- src/views/SnippetDetails.vue
+++ src/views/SnippetDetails.vue
@@ -18,7 +18,7 @@
         <font-awesome-icon
           icon="clone"
           class="icon"
-          @click="copyStep(index, step.text)"
+          @click="copyStep(index, 'any random text')"
         />
         <span :class="{ copy: activeElement === index }">Copied!</span>
       </Step>
